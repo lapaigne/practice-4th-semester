d71a7919ce04a52be3262be97bfb891a41211078
Oleg Lialin <lyalin.pa1nt3r@gmail.com>
2024-03-26 11:14:44 +0400

    todo: fix empty robot queue with id == 2

--- Roads/Roads.cs
+++ Roads/Roads.cs
@@ -16,16 +16,20 @@ namespace practice_4th_semester.Roads
             new Robot
             {
                 id = 1,
-                current = 4,
-                speed = 1
+                current = 1,
+                speed = 2
             },
-            //new Robot
-            //{
-            //    id = 2,
-            //    current = 0,
-            //    speed = 2
-            //}
+            new Robot
+            {
+                id = 2,
+                current = 0,
+                speed = 2
+            }
         ]);
+
+        static int robotCount = 3;
+
+        static double minTime = double.MaxValue;
 
         static int[,] network = { };
         
@@ -33,12 +37,7 @@ namespace practice_4th_semester.Roads
         {
             network = MakeNetwork(6);
 
-            for (int i = 0; i < 25; i++)
-            {
-                
-                //Console.WriteLine($"{first.id} {first.current} >> {first.next} {first.time} {first.distance}");
-                Step();
-            }
+            Step();
         }
 
         static int[,] MakeNetwork(int count = 6)
@@ -46,12 +45,12 @@ namespace practice_4th_semester.Roads
             
             int[][] list =
             [
-                [0, 1],
-                [0, 2],
-                [1, 2],
-                [1, 4],
-                [2, 3],
-                [3, 4],
+                [0, 1, 1],
+                //[0, 2, 1],
+                //[1, 2, 1],
+                //[1, 4, 1],
+                //[2, 3, 1],
+                //[3, 4, 1],
             ];
 
             var max = -1;
@@ -72,8 +71,8 @@ namespace practice_4th_semester.Roads
 
             for (int i = 0; i < list.Length; i++)
             {
-                network[list[i][0], list[i][1]] = 1;
-                network[list[i][1], list[i][0]] = 1;
+                network[list[i][0], list[i][1]] = list[i][2];
+                network[list[i][1], list[i][0]] = list[i][2];
             }
 
             return network;
@@ -81,86 +80,168 @@ namespace practice_4th_semester.Roads
 
         static void Step()
         {
-            var node = queue.Dequeue();
+            while (true)
+            {
+                var node = queue.Dequeue();
 
-            if (didMeet())
+                if (didMeet(3))
+                {
+                    break;
+                }
+
+                for (int i = 0; i < network.GetLength(0); i++)
+                {
+                    if (node.current >= network.GetLength(0))
+                    {
+                        Console.WriteLine("Роботы никогда не встретятся");
+                        return;
+                    }
+
+                    if (network[node.current, i] > 0)
+                    {
+                        if (node.next == -1) // если следующий город еще не выбран (только для начальных значений)
+                        {
+                            queue.Enqueue(
+                                new Robot
+                                {
+                                    id = node.id,
+                                    current = node.current,
+                                    time = node.time + .5,
+                                    distance = node.distance + .5 * node.speed,
+                                    next = i,
+                                    speed = node.speed
+                                }
+                            );
+                        }
+                        else if (node.distance == network[node.current, node.next]) // если достиг другого города, то выбирается новая точка
+                        {
+                            queue.Enqueue(
+                                new Robot
+                                {
+                                    id = node.id,
+                                    current = node.next,
+                                    time = node.time + .5,
+                                    distance = 0,
+                                    next = i,
+                                    speed = node.speed
+                                }
+                            );
+                        }
+                        else // если не достиг, то продолжает движение
+                        {
+                            queue.Enqueue(
+                                new Robot
+                                {
+                                    id = node.id,
+                                    current = node.current,
+                                    time = node.time + .5,
+                                    distance = node.distance + .5 * node.speed,
+                                    next = node.next,
+                                    speed = node.speed
+                                }
+                            );
+                        }
+                    }
+                }
+            }
+             
+            Console.WriteLine(minTime);
+
+            if (minTime > 0)
             {
                 return;
             }
 
-            for (int i = 0; i < network.GetLength(0); i++) 
-            {
-                if (network[node.current, i] > 0)
-                {
-                    if (node.next == -1) // если следующий город еще не выбран (только для начальных значений)
-                    {
-                        queue.Enqueue(
-                            new Robot
-                            {
-                                id = node.id,
-                                current = node.current,
-                                time = node.time + .5,
-                                distance = node.distance + .5 * node.speed,
-                                next = i,
-                                speed = node.speed
-                            }
-                        );
-                    }
-                    else if (node.distance == network[node.current, node.next]) // если достиг другого города, то выбирается новая точка
-                    {
-                        queue.Enqueue(
-                            new Robot
-                            {
-                                id = node.id,
-                                current = node.next,
-                                time = node.time + .5,
-                                distance = 0,
-                                next = i,
-                                speed = node.speed
-                            }
-                        );
-                    }
-                    else // если не достиг, то продолжает движение
-                    {
-                        queue.Enqueue( 
-                            new Robot {
-                                id = node.id,
-                                current = node.current,
-                                time = node.time + .5, 
-                                distance = node.distance + .5 * node.speed,
-                                next = node.next,
-                                speed = node.speed
-                            }
-                        );
-                    }
-                }
-            }
-            
+            Console.WriteLine("Роботы никогда не встретятся");
         }
 
-        static bool didMeet()
+        static bool didMeet(int count)
         {
+            var zeroth = queue.Where(robot => robot.id == 0);
+            var first = queue.Where(robot => robot.id == 1);
+            var second = queue.Where(robot => robot.id == 2);
+            
+            bool isThreeRobots = count == 3;
 
-            foreach (Robot robot0 in queue) {
-                foreach (Robot robot1 in queue)
+            foreach (Robot robot0 in zeroth) 
+            {
+                foreach (Robot robot1 in first)
                 {
-                    
-                    bool canMeet = robot0.id != robot1.id && robot0.time == robot1.time;
-                    bool didMeetAtCity = robot0.current == robot1.current;
-                    bool didMeetEnRoute = robot0.next == robot1.current && robot0.current == robot1.next;
-
-                    if (canMeet && (didMeetAtCity || didMeetEnRoute))
+                    Console.WriteLine(second.Count());
+                    if (isThreeRobots)
                     {
-                        if (didMeetAtCity)
+                        foreach (Robot robot2 in second)
                         {
-                            Console.WriteLine(robot0.time - 0.5);
+                            bool canMeet = robot0.time == robot1.time && robot0.time == robot2.time;
+                            bool didMeetAtCity = robot0.current == robot1.current && robot0.current == robot2.current && robot0.distance == 0 && robot1.distance == 0 && robot2.distance == 0;
+                            bool canMeetEnRoute = 
+                                robot0.current == robot1.next && 
+                                robot0.next == robot1.current && 
+                                (robot0.current == robot2.current&& robot0.next == robot2.next|| robot0.current == robot1.current && robot0.next == robot1.next);
+
+                            Console.WriteLine(canMeetEnRoute);
+                            if (canMeet)
+                            {
+                                
+                                if (canMeetEnRoute && (
+                                    robot0.speed != robot1.speed || 
+                                    robot0.speed != robot2.speed || 
+                                    robot1.speed != robot2.speed
+                                    ))
+                                {
+                                    var counter = 0;
+                                    for (int i = 0; i < network.GetLength(0); i++)
+                                    {
+                                        if (network[robot0.current, i] != 0 && network[robot0.next, i] != 0)
+                                        {
+                                            counter++;
+                                        }
+                                        if (counter == 1)
+                                        {
+                                            minTime = -1;
+                                            
+                                            return false;
+                                        }
+                                    }
+                                }
+
+                                if (didMeetAtCity)
+                                {
+                                    var time = robot0.time - 0.5;
+                                    minTime = time;
+                                    return true;
+                                }
+
+                                if (canMeetEnRoute)
+                                {
+                                    var time = (double)network[robot0.current, robot0.next] / (robot0.speed + robot1.speed);
+                                    minTime = time;
+                                    return true;
+                                }
+                            }
                         }
-                        else
+                    }
+                    else
+                    {
+                        bool canMeet = robot0.time == robot1.time;
+                        bool didMeetAtCity = robot0.current == robot1.current && robot0.distance == 0 && robot1.distance == 0;
+                        bool didMeetEnRoute = robot0.next == robot1.current && robot0.current == robot1.next;
+
+                        if (canMeet && (didMeetAtCity || didMeetEnRoute) )
                         {
-                            var time = (double)network[robot0.current, robot0.next] / (robot0.speed + robot1.speed);
-                            Console.WriteLine(time);
+                            if (didMeetAtCity)
+                            {
+                                var time = robot0.time - 0.5;
+                                minTime = time;
+                                return true;
+                            } 
+                            else
+                            {
+                                var time = (double)network[robot0.current, robot0.next] / (robot0.speed + robot1.speed);
+                                minTime = time;
+                                return true;
+                            }
                         }
-                        return true;
                     }
                 }
             }
